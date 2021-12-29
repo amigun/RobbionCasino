164ba018459de781a8b7e88c1754ba62f8c140a5
amigun <amigun.tube@gmail.com>
2021-12-29 12:11:17 +0500

    Add event on click "Play" button: send message with inline-buttons

--- handlers/main.py
+++ handlers/main.py
@@ -24,6 +24,8 @@ async def start(msg: types.Message, state: FSMContext):
 async def menu(msg: types.Message, state: FSMContext):
     if msg.text == messages.buttons['profile']:
         await msg.answer(messages.get['profile'].format(*db_queries.user_info(msg.from_user.id)))
+    elif msg.text == messages.buttons['play']:
+        await msg.answer(messages.get['select_mode'], reply_markup=kb.get['select_mode'])
 
 
 def register_handler(dp: Dispatcher):
--- keyboards.py
+++ keyboards.py
@@ -6,6 +6,11 @@ main_menu__kb = ReplyKeyboardMarkup(resize_keyboard=True)
 main_menu__kb.add(KeyboardButton(messages.buttons['play']),
                   KeyboardButton(messages.buttons['profile']))
 
+select_mode__kb = InlineKeyboardMarkup()
+select_mode__kb.add(InlineKeyboardButton(text=messages.buttons['solo'], callback_data='solo_mode'),
+                    InlineKeyboardButton(text=messages.buttons['duo'], callback_data='duo_mode'))
+
 get = {
     'main_menu': main_menu__kb,
+    'select_mode': select_mode__kb,
 }
